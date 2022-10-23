8172d7e474cdea66eca6e12e6b88237ab5d0ac11
Ajaz Rana <RanaAjaz@hotmail.com>
2022-10-23 02:16:34 -0400

    code clean up

--- GPT3-PreAuth.py
+++ GPT3-PreAuth.py
@@ -58,9 +58,9 @@ def upload_pdf( ):
 
         with open( uploaded_pdf.name, "rb" ) as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
+            pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
             
-            pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+            # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
             
             st.markdown( pdf_display, unsafe_allow_html = True )
             # print( type( base64_pdf ))
